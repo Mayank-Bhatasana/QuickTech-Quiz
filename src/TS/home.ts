c1c11c9542f52b355d6131fa24dd94a8e26464a9
RajKanjiya <24030401050@darshan.ac.in>
2026-01-01 14:09:26 +0530

    fix-bug-3

--- src/TS/home.ts
+++ src/TS/home.ts
@@ -3,9 +3,9 @@ import {supabase} from "../supabase/SupabaseClint.ts";
 
 //function to check user is auth or not
 (async function () {
-    const {data: {session}} = await supabase.auth.getSession();
+    const {data: {user}} = await supabase.auth.getUser();
 
-    if (!session) {
+    if (!user) {
         window.location.href = '../../src/Pages/login.html'
     }
 })()
